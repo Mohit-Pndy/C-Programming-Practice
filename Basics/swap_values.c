b8606d73776d85d738218a734d3780e749252606
Mohit <285234779+Mohit-Pndy@users.noreply.github.com>
2026-08-23 20:40:04 +0530

    Clean up code

--- Basics/swap_values.c
+++ Basics/swap_values.c
@@ -10,11 +10,12 @@ int main(){
     
     int temp_a = num1, temp_b = num2;
     
-    num1 = num1 + num2; // Store the sum of both numbers in num1.
-    num2 = num1 - num2; // Retrieve the original value of num1 into num2.
-    num1 = num1 - num2; // 
+    num1 = num1 + num2;
+    num2 = num1 - num2;
+    num1 = num1 - num2;
     
     printf("Original value of X & Y: %d, %d\n", temp_a, temp_b);
     printf("Swapped Value of X & Y: %d, %d\n", num1, num2);
+
     return 0;
 }
