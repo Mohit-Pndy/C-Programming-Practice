a64b44f32f49ac698a9c5065bbb3c278989e86a8
Mohit <285234779+Mohit-Pndy@users.noreply.github.com>
2026-08-27 10:26:40 +0530

    Add smallest of 3

---
+++ Conditional Statements/smallest_of_3.c
@@ -0,0 +1,25 @@
+// Q. Smallest of 3 numbers- Take 3 integer inputs from the user and print the smallest one.
+#include<stdio.h>
+int main(){
+    int num1,num2,num3;
+    
+    printf("enter 1st number: ");
+    scanf("%d", &num1);
+    
+    printf("enter 2nd number: ");
+    scanf("%d", &num2);
+    
+    printf("enter 3rd number: ");
+    scanf("%d", &num3);
+
+    int smallest = num1;
+
+    if (num2 < smallest)
+        smallest = num2;
+        
+    if (num3 < smallest)
+        smallest = num3;
+
+    printf("%d is the smallest one.\n", smallest);
+    return 0;
+}
