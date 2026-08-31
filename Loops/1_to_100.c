5abdb7a31341c752c37a259b401047ca506799f6
Mohit <285234779+Mohit-Pndy@users.noreply.github.com>
2026-08-31 22:35:49 +0530

    Add 1 to 100 count

---
+++ Loops/1_to_100.c
@@ -0,0 +1,10 @@
+// Q. Write a program to print numbers from 1 to 100 using a for loop.
+#include<stdio.h>
+int main(){
+    printf("Counting from 1 to 100: \n");
+    for(int i = 1; i <= 100; i++){
+        printf("%d ", i);
+    }
+    
+    return 0;
+}
